66a9c77ac637acddb7753380379f80a9961acfad
Christian Lopes Viana <christlv@trt3.jus.br>
2021-10-27 20:06:17 -0300

    Correções para devolver somente objetos alterados

--- api/controllers/reminders.js
+++ api/controllers/reminders.js
@@ -29,36 +29,40 @@ module.exports = app => {
   };
 
   controller.saveReminders = (req, res) => {
-    console.log("Saving " + req.body.note)
-    remindersMock.data.push({      
-      id: uuidv4(),
-      date: req.body.date,
-      time: req.body.time,
-      note: req.body.note,
-      color: req.body.color,
-      city: req.body.city      
-    });
-
-    res.status(201).json(remindersMock);
+    
+    const newReminder = {
+        id: uuidv4(),
+        date: req.body.date,
+        time: req.body.time,
+        note: req.body.note,
+        color: req.body.color,
+        city: req.body.city            
+    }
+    console.log(`Saving id=${newReminder.id}, date=${newReminder.date} time=${newReminder.time}, note=${newReminder.note}`); 
+    remindersMock.data.push(newReminder);
+    console.log(`Saved id=${newReminder.id}, date=${newReminder.date} time=${newReminder.time}, note=${newReminder.note}`); 
+    res.status(201).json(newReminder);
+    
   };
 
   controller.updateReminders = (req, res) => {
     const { 
       reminderId,
     } = req.params;
-    
+
+    console.log(`Updating id=${reminderId}`); 
+
     const foundReminderIndex = remindersMock.data.findIndex(reminder => reminder.id === reminderId);
 
     if (foundReminderIndex === -1) {
+      console.log(`Reminder id=${reminderId} not found`); 
       res.status(404).json({
         message: 'Reminder ' + reminderId + ' not found!',
-        success: false,
-        reminders: remindersMock,
+        success: false,        
       });
     } else {
 
-      console.log("Saving id =" + reminderId + ', color = ' + req.body.color); 
-      const newReminder = {
+      const updatedReminder = {
         id: reminderId,
         date: req.body.date,
         time: req.body.time,
@@ -67,12 +71,12 @@ module.exports = app => {
         city: req.body.city
       };
       
-      remindersMock.data.splice(foundReminderIndex, 1, newReminder);
-      
+      remindersMock.data.splice(foundReminderIndex, 1, updatedReminder);
+      console.log(`Updated id=${updatedReminder.id}, date=${updatedReminder.date} time=${updatedReminder.time}, note=${updatedReminder.note}`); 
       res.status(200).json({
         message: 'Reminder ' + reminderId + ' updated!',
         success: true,
-        reminders: remindersMock,
+        reminder: updatedReminder,
       });
     }
   }
@@ -82,20 +86,22 @@ module.exports = app => {
       reminderId,
     } = req.params;
 
+    console.log(`Deleting id=${reminderId}`); 
+
     const foundReminderIndex = remindersMock.data.findIndex(reminder => reminder.id === reminderId);
 
     if (foundReminderIndex === -1) {
+      console.log(`Reminder id=${reminderId} not found`); 
       res.status(404).json({
         message: 'Reminder not found.',
-        success: false,
-        reminders: remindersMock,
+        success: false,       
       });
     } else {
       remindersMock.data.splice(foundReminderIndex, 1);
+      console.log(`Deleted id=${reminderId}`); 
       res.status(200).json({
         message: 'Reminder found and deleted sucessfully!',
-        success: true,
-        reminders: remindersMock,
+        success: true
       });
     }
   };
